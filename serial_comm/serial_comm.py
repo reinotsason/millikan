823cc074ca038ca66152f36a38791f3710655661
Rein Otsason <reinotsason@gmail.com>
2016-12-20 14:00:49 -0500

    Use real voltages in serial_comm
    The get_voltage and send_voltage methods of the VoltageController now
    return and accept real voltages, instead of DAC levels.
    
    The object now accepts keyword arguments to adjust the calibration.

--- serial_comm/serial_comm.py
+++ serial_comm/serial_comm.py
@@ -4,7 +4,9 @@ import serial
 class VoltageController(object):
     """ Class to represent the voltage controller """
 
-    def __init__(self, port='/dev/ttyACM0', rate=115200):
+    def __init__(self, port='/dev/ttyACM0', rate=115200,
+                 write_max_scale=975, write_min_scale=3,
+                 read_max_scale=950, read_min_scale=0):
         """ Set up the voltage controller.
 
         Keyword arguments:
@@ -13,6 +15,11 @@ class VoltageController(object):
         """
 
         self.ser = serial.Serial(port=port, baudrate=rate)
+        self.write_max_scale = write_max_scale
+        self.write_min_scale = write_min_scale
+        self.read_max_scale = read_max_scale
+        self.read_min_scale = read_min_scale
+
 
     def send_voltage(self, voltage):
         """ Set the voltage on the controller.
@@ -21,7 +28,15 @@ class VoltageController(object):
         voltage -- voltage to be set in volts
         """
 
-        text = 'v' + str(voltage)
+        voltage_level = (voltage - self.write_min_scale) / (self.write_max_scale - self.write_min_scale)
+        voltage_level = int(voltage_level)
+
+        if voltage_level > 4095:
+            voltage_level = 4095
+        elif voltage_level < 0:
+            voltage_level = 0
+
+        text = 'v' + str(voltage_level)
         data = text.encode(encoding='ascii')
         self.ser.write(data)
 
@@ -34,6 +49,8 @@ class VoltageController(object):
         data = 'r'.encode(encoding='ascii')
         self.ser.write(data)
         bytes_to_read = self.ser.in_waiting
-        voltage_level = self.ser.read(bytes_to_read)
+        voltage_level = int(self.ser.read(bytes_to_read))
 
-        return voltage_level
+        voltage = self.read_min_scale + (voltage_level / 4095) * (self.read_max_scale - self.read_min_scale)
+
+        return voltage
